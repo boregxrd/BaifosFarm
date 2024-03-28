9ca9bcb6a5cae113f3338affd252fc51177cc7f7
Elena <esowesp@epsg.upv.es>
2024-03-28 19:29:52 +0100

    se crea heno en tiempo de ejecución

--- Assets/__test__/RecogerHeno/RecogerHeno.cs
+++ Assets/__test__/RecogerHeno/RecogerHeno.cs
@@ -11,6 +11,12 @@ public class RecogerHeno : MonoBehaviour
 
     private GameObject objetoEnMano = null;
 
+    public GameObject prefabHeno;
+
+    public GameObject heno;
+    
+    public BoxCollider colliderHeno;
+
    
     void Update()
     {
@@ -24,12 +30,23 @@ public class RecogerHeno : MonoBehaviour
         {
             if (Input.GetKey("e") && objetoEnMano == null)
             {
+                heno = Instantiate(prefabHeno);
+            
+                //heno.transform.position = new Vector3(0, 0, 0);
+                heno.GetComponent<Rigidbody>().useGravity = false;
+                heno.GetComponent<Rigidbody>().isKinematic = true;
+                heno.transform.position = puntoDeMano.transform.position;
+                heno.transform.SetParent(puntoDeMano.transform);
+                objetoEnMano = other.gameObject;
+
                 //crear objeto heno
+                /*
                 other.GetComponent<Rigidbody>().useGravity = false;
                 other.GetComponent <Rigidbody>().isKinematic = true;
                 other.transform.position = puntoDeMano.transform.position;
                 other.gameObject.transform.SetParent(puntoDeMano.transform);
                 objetoEnMano = other.gameObject;
+                */
 
             }
         }
