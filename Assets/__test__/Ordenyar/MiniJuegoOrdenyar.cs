16ae0e3c2ce5cbf18e50364bdf0687c52db83c23
Elena <esowesp@epsg.upv.es>
2024-03-30 21:52:33 +0100

    se genera la leche

--- Assets/__test__/Ordenyar/MiniJuegoOrdenyar.cs
+++ Assets/__test__/Ordenyar/MiniJuegoOrdenyar.cs
@@ -13,20 +13,74 @@ public class MiniJuegoOrdenyar : MonoBehaviour
     private float valorMaximo = 100f;
     [SerializeField] private float valorActual = 15f;
     [SerializeField] private float velocidadVaciado = 5f;
-    public bool lechePreparada = false;
+    [SerializeField] private float incremento = 15f;
 
     [SerializeField] private Image barraOrdenyar;
-    
+
+    [SerializeField] private ControladorAccionesPersonaje controladorAccionesPersonaje;
+    [SerializeField] private GameObject prefabLeche;
+    [SerializeField] private GameObject leche;
 
 
     void Start()
     {
-        objetoMiniJuegoOrdenyar.SetActive(true);
-        barraOrdenyar.fillAmount = valorActual / valorMaximo; // Asegúrate de que la barra se inicialice correctamente
+        objetoMiniJuegoOrdenyar.SetActive(false);
+        barraOrdenyar.fillAmount = valorActual / valorMaximo;
     }
 
     private void Update()
     {
-        
+
+        VaciarConElTiempo();
+
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            objetoMiniJuegoOrdenyar.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            incrementar();
+        }
+
+        if(valorActual >= valorMaximo)
+        {
+            valorActual = valorMaximo;
+            objetoMiniJuegoOrdenyar.SetActive(false);
+            generarLeche();
+        }
+
+    }
+
+    private void incrementar()
+    {
+        valorActual += incremento;
+        barraOrdenyar.fillAmount = valorActual / valorMaximo;
+    }
+
+    private void VaciarConElTiempo()
+    {
+        if (valorActual > 0)
+        {
+            valorActual -= velocidadVaciado * Time.deltaTime;
+            barraOrdenyar.fillAmount = valorActual / valorMaximo; // Actualiza la barra de alimentación visualmente
+        }
+        else //si la barra llega a 0
+        {
+            valorActual = 0;
+            objetoMiniJuegoOrdenyar.SetActive(false);
+        }
+    }
+
+    private void generarLeche()
+    {
+        leche = Instantiate(prefabLeche);
+
+        leche.GetComponent<Rigidbody>().useGravity = false;
+        leche.GetComponent<Rigidbody>().isKinematic = true;
+
+        leche.transform.position = controladorAccionesPersonaje.puntoDeMano.transform.position;
+        leche.transform.SetParent(controladorAccionesPersonaje.puntoDeMano.transform);
+        controladorAccionesPersonaje.objetoEnMano = leche;
     }
 }
