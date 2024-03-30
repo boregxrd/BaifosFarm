4b6467b00f92df0defa1efaeffe30212895e7dd8
Elena <esowesp@epsg.upv.es>
2024-03-30 13:32:12 +0100

    interfaz mini juego ordenyar creada

---
+++ Assets/__test__/Ordenyar/MiniJuegoOrdenyar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniJuegoOrdenyar : MonoBehaviour
+{
+    [SerializeField] private GameObject objetoMiniJuegoOrdenyar;
+
+    void Start()
+    {
+        objetoMiniJuegoOrdenyar.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
