9d63c1a92286ff39b340ef12c05d17ac941022fa
mluimes <mluismesa@espg.upv.es>
2024-04-02 19:18:09 +0100

    Reseteo numero de cabras al iniciar nueva partida

--- Assets/[BaifosFarm]/Menus/MenuInicio/MenuInicio.cs
+++ Assets/[BaifosFarm]/Menus/MenuInicio/MenuInicio.cs
@@ -6,10 +6,15 @@ using UnityEngine.SceneManagement;
 public class MenuInicio : MonoBehaviour
 {
     public void Jugar() {
+        // Reset valores de cabras para nueva partida
+        PlayerPrefs.SetInt("cabrasBlancas", 0);
+        PlayerPrefs.SetInt("cabrasNegras", 0);
         SceneManager.LoadScene("Juego");
     }
 
-    public void Ajustes() {}
+    public void Ajustes() {
+        Debug.Log("Falta por hacer el menu ajustes");
+    }
 
     public void Salir() {
         Application.Quit();
