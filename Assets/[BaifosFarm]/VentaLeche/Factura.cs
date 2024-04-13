9e3a19455a59a54e8df483887bd2f00292b733ee
JuanDiaz <juanben10xlr99@gmail.com>
2024-04-13 17:47:12 +0200

    Factura Gasto Heno Done and Cleaned

--- Assets/[BaifosFarm]/VentaLeche/Factura.cs
+++ Assets/[BaifosFarm]/VentaLeche/Factura.cs
@@ -9,51 +9,36 @@ public class Factura : MonoBehaviour
     public Text txtFactura;
     public int cabrasNuevas;
     public SistemaMonetario sistemaMonetario; // Referencia al Singleton del SistemaMonetario
-
     private void Awake()
     {
         Debug.Log("INICIO FACTURA");
         cabrasNuevas = 0;
-        // sistemaMonetario = SistemaMonetario.Instance; // Obtener la instancia del Singleton
         ActualizarTexto();
     }
 
     public void comprarCabra()
     {
-        int costoCabra = 20; // Definir el costo de la cabra aquí
-        // Verificar si el jugador tiene suficiente dinero para comprar una cabra
+        int costoCabra = 20;
         if (PlayerPrefs.GetInt("DineroTotal", 0) >= costoCabra)
         {
-            // Restar el costo de la cabra del dinero total
             sistemaMonetario.RestarDinero(costoCabra);
-
-            // Realizar cualquier acción adicional necesaria para agregar la cabra al jugador
-            Debug.Log("¡Has comprado una cabra!");
 
             // Get valores de PlayerPrefs
             int numCabrasBlancas = PlayerPrefs.GetInt("cabrasBlancas", 0);
             int numCabrasNegras = PlayerPrefs.GetInt("cabrasNegras", 0);
-            Debug.Log("GET DONE: " + numCabrasBlancas + ", " + numCabrasNegras);
-            Debug.Log("Dinero total: $" + PlayerPrefs.GetInt("DineroTotal", 0));
 
             // comprobar si hay cabra negra y 10% de que salga 
             if (numCabrasNegras == 0 && Random.value <= 0.1f)
             {
-                Debug.Log("salio negra");
-                // si sale añadir cabra negra 
                 numCabrasNegras++;
             }
-            // si no sale o si ya hay negra anyadir blanca
             else
             {
-                Debug.Log("se añade blanca");
                 numCabrasBlancas++;
             }
-            // añadir cabras nuevas a sus PlayerPrefs'
             PlayerPrefs.SetInt("cabrasBlancas", numCabrasBlancas);
             PlayerPrefs.SetInt("cabrasNegras", numCabrasNegras);
-            Debug.Log("SET DONE: " + PlayerPrefs.GetInt("cabrasBlancas", 0) + ", " + PlayerPrefs.GetInt("cabrasNegras", 0));
-            cabrasNuevas++; // variable para factura
+            cabrasNuevas++;
             ActualizarTexto();
         }
         else
@@ -66,6 +51,7 @@ public class Factura : MonoBehaviour
     public void continuar()
     {
         SceneManager.LoadScene("Juego");
+        sistemaMonetario.RestarDinero(sistemaMonetario.CalcularGastoHeno());
     }
 
     private void ActualizarTexto()
@@ -76,7 +62,9 @@ public class Factura : MonoBehaviour
         {
             txtFactura.text += "\nCabras nuevas - " + cabrasNuevas.ToString();
         }
+
         // Actualizar el dinero total en el texto
         txtFactura.text += "\nDinero total - $" + PlayerPrefs.GetInt("DineroTotal", 0).ToString();
+        txtFactura.text += "\nGasto heno siguiente día - $" + sistemaMonetario.CalcularGastoHeno().ToString();
     }
 }
