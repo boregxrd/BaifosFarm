5e797917eed92ec37e0b50ea788fdac6e8d778a1
Alex <alexeltren@gmail.com>
2024-04-17 21:12:59 +0200

    Menu Ajustes Actualizado Funcional
    Para que pueda cerrarse sin problemas

--- Assets/[BaifosFarm]/Menus/MenuAjustes/MenuAjustes.cs
+++ Assets/[BaifosFarm]/Menus/MenuAjustes/MenuAjustes.cs
@@ -6,11 +6,13 @@ public class MenuAjustes : MonoBehaviour
 {
     public AudioMixer audioMixer; // AudioMixer para controlar el volumen
     public Slider sliderVolumen; // Slider para ajustar el volumen
+    [SerializeField] private GameObject GrupoMenuAjustes; // Referencia al Canvas del menú de ajustes
+
 
     void Start()
     {
         // Desactivar el GameObject del menú de ajustes al iniciar el juego
-
+        GrupoMenuAjustes.SetActive(false);
         // Obtener el valor actual del volumen y actualizar el slider
         float volumenActual;
         bool resultado = audioMixer.GetFloat("Volumen", out volumenActual);
@@ -26,5 +28,11 @@ public class MenuAjustes : MonoBehaviour
     {
         Debug.Log(volumen);
         audioMixer.SetFloat("Volumen", volumen); // Establecer el volumen en el AudioMixer
+    }
+
+    // Método para cerrar el menú de ajustes
+    public void CerrarMenuAjustes()
+    {
+        GrupoMenuAjustes.SetActive(false); // Desactivar el Canvas del menú de ajustes
     }
 }
