f9f7a4ad90c97798eb8ce65d2730c7a6db29203c
Alex <alexeltren@gmail.com>
2024-04-17 20:58:20 +0200

    Menu Ajustes Modificado Funcional

--- Assets/[BaifosFarm]/Menus/MenuAjustes/MenuAjustes.cs
+++ Assets/[BaifosFarm]/Menus/MenuAjustes/MenuAjustes.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuAjustes : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioMixer audioMixer; // AudioMixer para controlar el volumen
+    public Slider sliderVolumen; // Slider para ajustar el volumen
+
     void Start()
     {
-        
+        // Desactivar el GameObject del menú de ajustes al iniciar el juego
+
+        // Obtener el valor actual del volumen y actualizar el slider
+        float volumenActual;
+        bool resultado = audioMixer.GetFloat("Volumen", out volumenActual);
+
+        if (resultado)
+        {
+            sliderVolumen.value = volumenActual;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Método para ajustar el volumen
+    public void AjustarVolumen(float volumen)
     {
-        
+        Debug.Log(volumen);
+        audioMixer.SetFloat("Volumen", volumen); // Establecer el volumen en el AudioMixer
     }
 }
