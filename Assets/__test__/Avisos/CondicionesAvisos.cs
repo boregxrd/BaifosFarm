5b3d81c175ec9535dd150e8adda44c3fca074641
Elena <esowesp@epsg.upv.es>
2024-04-18 20:48:26 +0200

    condicionesDeAvisos funciona

--- Assets/__test__/Avisos/CondicionesAvisos.cs
+++ Assets/__test__/Avisos/CondicionesAvisos.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class CondicionesAvisos : MonoBehaviour
 {
 
-    [SerializeField] private Cabra[] cabrasEnEscena;
+    [SerializeField] private List<Cabra> cabrasEnEscena;
     private LimitesCamara limitesCamara;
+
+    private float valorAlertaHambre = 30f;
+    private float valorAlertaMuerte = 5f;
+    private float valorLecheCompleta = 100f;
 
     private void Awake()
     {
@@ -15,7 +19,7 @@ public class CondicionesAvisos : MonoBehaviour
 
     private void Update()
     {
-        if(cabrasEnEscena.Length == 0) //para que solo ejecute este código una vez
+        if(cabrasEnEscena.Count == 0) //para que solo ejecute este código una vez
         {
             cabrasEnEscena = ObtenerCabrasDeEscena(); //las cabras se crean después de start() por eso lo he puesto en update(), sería mejor cambiar esto en un futuro
         }
@@ -26,23 +30,57 @@ public class CondicionesAvisos : MonoBehaviour
 
     }
 
-    private Cabra[] ObtenerCabrasDeEscena()
+    private List<Cabra> ObtenerCabrasDeEscena()
     {
+        // Inicializa una lista para almacenar las cabras
+        List<Cabra> cabrasEnEscena = new List<Cabra>();
+
+        // Encuentra todos los objetos en la escena que tengan el script Cabra adjunto
         Cabra[] cabrasEncontradas = FindObjectsOfType<Cabra>();
 
-        return cabrasEncontradas;
+        // Agrega cada cabra encontrada a la lista
+        foreach (Cabra cabra in cabrasEncontradas)
+        {
+            cabrasEnEscena.Add(cabra);
+        }
+
+        return cabrasEnEscena;
     }
 
     private void comprobarDondeEstanLasCabras()
     {
+        
         foreach (Cabra cabra in cabrasEnEscena)
         {
-            bool estaFueraDeCamara = limitesCamara.ObjetoFueraDeCamara(cabra.transform.position);
-            if (estaFueraDeCamara)
+            if (cabra != null)
             {
-                //comprobar nivel de todo
-                Debug.Log($"{cabra.name} está fuera de camara");
+                bool estaFueraDeCamara = limitesCamara.ObjetoFueraDeCamara(cabra.transform.position);
+
+                if (estaFueraDeCamara)
+                {
+                    if(cabra.nivelDeAlimentacion() <= valorAlertaHambre && 
+                        cabra.nivelDeAlimentacion() > valorAlertaMuerte)
+                    {
+                        Debug.Log($"{cabra.name} tiene hambre");
+                    }
+
+                    if (cabra.nivelDeAlimentacion() <= valorAlertaMuerte)
+                    {
+                        Debug.Log($"{cabra.name} va a morir");
+                    }
+
+                    if (cabra.nivelDeLeche() == valorLecheCompleta)
+                    {
+                        Debug.Log($"{cabra.name} tiene LECHE");
+                    }
+              
+                }
             }
+            else
+            {
+                cabrasEnEscena.Clear();
+            }
+           
             
         }
     }
