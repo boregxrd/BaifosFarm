9596cde0d1bb81a9508d752b432a543d8268f0b0
Alex <alexeltren@gmail.com>
2024-04-20 20:24:43 +0200

    Script Funcionamiento Tutorial

---
+++ Assets/[BaifosFarm]/[Scenes]/Tutorial/TutorialManager.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    public ParticleSystem[] particleEffects; // Efectos de partículas para cada paso
+
+    private int popUpIndex;
+    [SerializeField] private RecogerAlimento recogerAlimento;
+    [SerializeField] private Alimentar alimentar;
+    [SerializeField] private Ordeniar ordeniar;
+    [SerializeField] private DejarLecheEnCaja dejarLecheEnCaja;
+    [SerializeField] private Character movimientoPersonaje;
+
+    private void Start()
+    {
+        ShowNextPopUp();
+    }
+
+    private void Update()
+    {
+        // Aquí solo mantienes la lógica de verificación de pasos
+        CheckCompletion();
+    }
+
+    private void ShowNextPopUp()
+    {
+        // Ocultar todos los pop-ups y desactivar los efectos de partículas
+        foreach (var popUp in popUps)
+        {
+            popUp.SetActive(false);
+        }
+
+        foreach (var effect in particleEffects)
+        {
+            effect.Stop();
+        }
+
+        if (popUpIndex < popUps.Length)
+        {
+            // Activar el efecto de partículas correspondiente
+            particleEffects[popUpIndex].Play();
+
+            // Mostrar el pop-up
+            popUps[popUpIndex].SetActive(true);
+            Debug.Log($"Mostrando pop-up {popUpIndex + 1}"); // Mensaje de depuración
+        }
+        else
+        {
+            PlayerPrefs.SetInt("TutorialCompleto", 1); // Marcar el tutorial como completado
+            Debug.Log("Tutorial completado");
+        }
+    }
+
+    private void CheckCompletion()
+    {
+        switch (popUpIndex)
+        {
+            case 0: // Movimiento
+                if (movimientoPersonaje.HasMoved())
+                {
+                    CompleteStep();
+                    Debug.Log("Movimiento completado");
+                }
+                break;
+
+            case 1: // Recoger Heno
+                if (recogerAlimento.henoRecogido)
+                {
+                    CompleteStep();
+                    Debug.Log("Recoger Heno completado");
+                }
+                break;
+
+            case 2: // Alimentar
+                if (alimentar.alimentacionRealizada)
+                {
+                    CompleteStep();
+                    Debug.Log("Alimentar completado");
+                }
+                break;
+
+            case 3: // Recoger Leche
+                if (ordeniar.ordeñoIniciado)
+                {
+                    CompleteStep();
+                    Debug.Log("Recoger Leche completado");
+                }
+                break;
+
+            case 4: // Guardar Leche
+                if (dejarLecheEnCaja.lecheGuardada)
+                {
+                    CompleteStep();
+                    Debug.Log("Guardar Leche completado");
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void CompleteStep()
+    {
+        // Detener el efecto de partículas actual
+        particleEffects[popUpIndex].Stop();
+
+        // Pasar al siguiente pop-up
+        NextPopUp();
+    }
+
+    private void NextPopUp()
+    {
+        // Ocultar el pop-up actual
+        popUps[popUpIndex].SetActive(false);
+
+        // Incrementar el índice del pop-up
+        popUpIndex++;
+
+        // Mostrar el siguiente pop-up
+        ShowNextPopUp();
+    }
+}
