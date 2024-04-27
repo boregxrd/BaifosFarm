225702005f9297605f88426631e7d3962a16a1e9
JuanDiaz <juanben10xlr99@gmail.com>
2024-04-27 20:47:07 +0200

    Borrar Codigo innecesario control dinero en codigo caja leche

--- Assets/[BaifosFarm]/CajaLeche/ControladorTextoCaja.cs
+++ Assets/[BaifosFarm]/CajaLeche/ControladorTextoCaja.cs
@@ -9,12 +9,8 @@ public class ControladorTextoCaja : MonoBehaviour
     [SerializeField] private ControladorAccionesPersonaje controladorAccionesPersonaje;
     [SerializeField] private TextMeshPro textoCaja;
     [SerializeField] private SistemaMonetario sistemaMonetario; // Agrega referencia al SistemaMonetario
-    public Text textoDinero; // Referencia al objeto de texto que mostrar� el dinero 
     [SerializeField] int PRECIO_POR_BOTELLA = 10;
 
-    private void Awake() {
-        textoDinero.text = "Dinero: $" + PlayerPrefs.GetInt("DineroTotal", 0);
-    }
     void Update()
     {
         int botellasObtenidas = controladorAccionesPersonaje.lechesGuardadas;
@@ -26,14 +22,5 @@ public class ControladorTextoCaja : MonoBehaviour
         int botellasObtenidas = controladorAccionesPersonaje.lechesGuardadas;
         int dineroGanado = botellasObtenidas * PRECIO_POR_BOTELLA; // Cada botella vale $10
         sistemaMonetario.AgregarDinero(dineroGanado);
-    
-        if (textoDinero != null)
-        {
-            textoDinero.text = "Dinero: $" + PlayerPrefs.GetInt("DineroTotal", 0).ToString();
-        }
-        else
-        {
-            Debug.LogWarning("El objeto de texto para el dinero total no est� asignado en el inspector.");
-        }
     }
 }
