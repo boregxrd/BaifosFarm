e9b0cc5ea2ae049f41b25b2d974352579733fa7f
Elena <esowesp@epsg.upv.es>
2024-05-09 21:37:23 +0200

    aviso leche nuevo funciona
    Solo se mueve la flecha y no toda la imagen del aviso

--- Assets/[BaifosFarm]/Avisos/ControlAvisos.cs
+++ Assets/[BaifosFarm]/Avisos/ControlAvisos.cs
@@ -36,7 +36,7 @@ public class ControlAvisos : MonoBehaviour
         }
 
         // Actualizar la posición y orientación del aviso
-        RectTransform rectTransform = avisoActual.GetComponent<RectTransform>();
+        RectTransform rectTransform = avisoActual.transform.GetChild(0).GetComponent<RectTransform>();
         rectTransform.anchoredPosition = Vector2.zero;
         rectTransform.localScale = Vector3.one;
         rectTransform.localRotation = Quaternion.identity;
@@ -45,15 +45,18 @@ public class ControlAvisos : MonoBehaviour
 
     private void ActualizarAviso(RectTransform avisoRectTransform, Vector3 posicionCabra)
     {
+        RectTransform flechaAviso = avisoRectTransform.GetChild(0).GetComponent<RectTransform>();
+
         Vector3 posicionPantalla = camara.WorldToScreenPoint(posicionCabra);
         Vector3 desde = camara.WorldToScreenPoint(camara.transform.position);
         desde.z = 0;
 
         Vector3 direccion = (posicionPantalla - desde).normalized;
         float angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
-        avisoRectTransform.localEulerAngles = new Vector3(0, 0, angulo);
+        flechaAviso.localEulerAngles = new Vector3(0, 0, angulo);
 
         LimitarPosicionAviso(avisoRectTransform, posicionCabra);
+        
     }
 
     private void LimitarPosicionAviso(RectTransform avisoRectTransform, Vector3 posicionObjetivo)
