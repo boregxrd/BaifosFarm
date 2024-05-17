cd9d25979078c889cba089f751c6f9e9c9e75c89
JuanDiaz <juanben10xlr99@gmail.com>
2024-05-17 15:03:27 +0200

    Comic Intro Cursor

--- Assets/[BaifosFarm]/Tutorial/Introduccion Comic/ComicIntroController.cs
+++ Assets/[BaifosFarm]/Tutorial/Introduccion Comic/ComicIntroController.cs
@@ -4,62 +4,64 @@ using UnityEngine.UI;
 
 public class ComicIntroController : MonoBehaviour
 {
-    public Image[] comicImages; // Array de imágenes del cómic
-    public Button continueButton; // Botón de continuar
+    public Image[] comicImages; // Array de imï¿½genes del cï¿½mic
+    public Button continueButton; // Botï¿½n de continuar
 
-    private int currentIndex = 0; // Índice actual de la imagen mostrada
+    private int currentIndex = 0; // ï¿½ndice actual de la imagen mostrada
 
+    public Texture2D cursorMano; // Textura del cursor de mano
+    public Texture2D cursorNormal; // Textura del cursor normal
     private void Start()
     {
-        // Desactivar el botón de continuar al inicio
+        // Desactivar el botï¿½n de continuar al inicio
         continueButton.gameObject.SetActive(false);
 
-        // Mostrar el puntero del ratón al inicio
+        // Mostrar el puntero del ratï¿½n al inicio
         //Cursor.visible = true;
 
         if (PlayerPrefs.GetInt("TutorialCompleto") == 0) 
         { 
-            // Iniciar la secuencia de mostrar imágenes del cómic
+            // Iniciar la secuencia de mostrar imï¿½genes del cï¿½mic
             StartCoroutine(ShowComicSequence());
         }
     }
 
-    // Método para mostrar la secuencia de imágenes del cómic
+    // Mï¿½todo para mostrar la secuencia de imï¿½genes del cï¿½mic
     private IEnumerator ShowComicSequence()
     {
         Time.timeScale = 0f; // Pausar el tiempo del juego
 
         foreach (Image image in comicImages)
         {
-            // Mostrar la imagen actual del cómic
+            // Mostrar la imagen actual del cï¿½mic
             image.gameObject.SetActive(true);
             //Debug.Log($"Mostrada la imagen {currentIndex + 1}");
 
             if (currentIndex == 3)
             {
-                // Mostrar el botón de continuar junto con la cuarta imagen
+                // Mostrar el botï¿½n de continuar junto con la cuarta imagen
                 continueButton.gameObject.SetActive(true);
-                //Debug.Log("Mostrando botón de continuar");
+                //Debug.Log("Mostrando botï¿½n de continuar");
 
-                // Ocultar el puntero del ratón al mostrar el botón de continuar
+                // Ocultar el puntero del ratï¿½n al mostrar el botï¿½n de continuar
                 //Cursor.visible = false;
             }
 
             yield return new WaitForSecondsRealtime(3f); // Esperar 3 segundos
 
-            currentIndex++; // Avanzar al siguiente índice
+            currentIndex++; // Avanzar al siguiente ï¿½ndice
         }
 
-        // Mostrar el puntero del ratón cuando se completa la secuencia del cómic
+        // Mostrar el puntero del ratï¿½n cuando se completa la secuencia del cï¿½mic
         //Cursor.visible = true;
     }
 
-    // Método llamado por el botón de continuar
+    // Mï¿½todo llamado por el botï¿½n de continuar
     public void OnContinueButtonClicked()
     {
-        Debug.Log("Botón de continuar pulsado");
+        Debug.Log("Botï¿½n de continuar pulsado");
 
-        // Ocultar todas las imágenes y el botón de continuar
+        // Ocultar todas las imï¿½genes y el botï¿½n de continuar
         foreach (Image image in comicImages)
         {
             image.gameObject.SetActive(false);
@@ -67,5 +69,17 @@ public class ComicIntroController : MonoBehaviour
         continueButton.gameObject.SetActive(false);
 
         Time.timeScale = 1f; // Reanudar el tiempo del juego
+    }
+
+    public void OnButtonCursorEnter()
+    {
+        // Cambiar el cursor a mano
+        Cursor.SetCursor(cursorMano, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnButtonCursorExit()
+    {
+        // Cambiar el cursor a normal
+        Cursor.SetCursor(cursorNormal, Vector2.zero, CursorMode.Auto);
     }
 }
