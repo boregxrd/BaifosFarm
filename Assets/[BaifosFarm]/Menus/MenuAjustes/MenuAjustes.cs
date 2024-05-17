3d897487f1983cb9a06dba2ab9333748d083a10f
JuanDiaz <juanben10xlr99@gmail.com>
2024-05-17 15:03:02 +0200

    Ajustes Cursor

--- Assets/[BaifosFarm]/Menus/MenuAjustes/MenuAjustes.cs
+++ Assets/[BaifosFarm]/Menus/MenuAjustes/MenuAjustes.cs
@@ -6,6 +6,8 @@ public class MenuAjustes : MonoBehaviour
 {
     public AudioMixer audioMixer; // AudioMixer para controlar el volumen
     public Slider sliderVolumen; // Slider para ajustar el volumen
+    public Texture2D cursorMano; // Textura del cursor de mano
+    public Texture2D cursorNormal; // Textura del cursor normal
     [SerializeField] private GameObject GrupoMenuAjustes; // Referencia al Canvas del menu de ajustes
     [SerializeField] private GameObject objetoMenuPausa;
 
@@ -36,5 +38,17 @@ public class MenuAjustes : MonoBehaviour
     {
         GrupoMenuAjustes.SetActive(false); // Desactivar el Canvas del menu de ajustes
         if(objetoMenuPausa != null) objetoMenuPausa.SetActive(true);
+    }
+
+    public void OnButtonCursorEnter()
+    {
+        // Cambiar el cursor a mano
+        Cursor.SetCursor(cursorMano, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnButtonCursorExit()
+    {
+        // Cambiar el cursor a normal
+        Cursor.SetCursor(cursorNormal, Vector2.zero, CursorMode.Auto);
     }
 }
