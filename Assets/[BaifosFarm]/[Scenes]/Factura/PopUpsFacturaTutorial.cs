1c56be1aa5140846508475d05dbc7826fea287b0
JuanDiaz <juanben10xlr99@gmail.com>
2024-05-20 16:28:50 +0200

    Interactuable Boton Ok Factura Tutorial

--- Assets/[BaifosFarm]/[Scenes]/Factura/PopUpsFacturaTutorial.cs
+++ Assets/[BaifosFarm]/[Scenes]/Factura/PopUpsFacturaTutorial.cs
@@ -5,34 +5,34 @@ using UnityEngine.UI;
 public class PopUpsFacturaTutorial : MonoBehaviour
 {
     public GameObject[] popUps; // Array de pop-ups
-    public Button okButton; // Botón de OK que será asignado en el inspector
+    public Button okButton; // Botï¿½n de OK que serï¿½ asignado en el inspector
 
-    private bool isButtonClicked = false; // Flag para saber si el botón ha sido clicado
+    private bool isButtonClicked = false; // Flag para saber si el botï¿½n ha sido clicado
 
     private void Start()
     {
-        // Asegurarse de que el botón esté desactivado al inicio
+        // Asegurarse de que el botï¿½n estï¿½ desactivado al inicio
         //okButton.gameObject.SetActive(false);
         okButton.onClick.AddListener(OnOkButtonClicked);
     }
 
-    // Método que maneja la acción del botón OK
+    // Mï¿½todo que maneja la acciï¿½n del botï¿½n OK
     private void OnOkButtonClicked()
     {
-        isButtonClicked = true; // Marcar que el botón ha sido clicado
-        okButton.gameObject.SetActive(false); // Ocultar el botón OK
+        isButtonClicked = true; // Marcar que el botï¿½n ha sido clicado
+        okButton.gameObject.SetActive(false); // Ocultar el botï¿½n OK
     }
 
     // Coroutine que maneja cada pop-up individualmente
     private IEnumerator HandlePopUp(GameObject popUp)
     {
-        isButtonClicked = false; // Resetear el flag del botón clicado
+        isButtonClicked = false; // Resetear el flag del botï¿½n clicado
         popUp.SetActive(true); // Mostrar el pop-up
-        okButton.gameObject.SetActive(true); // Mostrar el botón OK
+        okButton.gameObject.SetActive(true); // Mostrar el botï¿½n OK
 
         Debug.Log($"Mostrado pop-up: {popUp.name}");
 
-        // Esperar hasta que el botón OK se haya clicado
+        // Esperar hasta que el botï¿½n OK se haya clicado
         yield return new WaitUntil(() => isButtonClicked);
 
         popUp.SetActive(false); // Ocultar el pop-up
@@ -51,13 +51,13 @@ public class PopUpsFacturaTutorial : MonoBehaviour
         PlayerPrefs.SetInt("TutorialCompleto", 1); // Marcar el tutorial como completado
     }
 
-    // Método para ocultar todos los pop-ups
+    // Mï¿½todo para ocultar todos los pop-ups
     public void HidePopUps()
     {
         foreach (var popup in popUps)
         {
             popup.SetActive(false);
         }
-        okButton.gameObject.SetActive(false); // Asegurarse de que el botón OK esté desactivado
+        okButton.gameObject.SetActive(false); // Asegurarse de que el botï¿½n OK estï¿½ desactivado
     }
 }
