1cd894e24334d6167d3e7f7f665e8c342df217ed
Elena <esowesp@epsg.upv.es>
2024-05-27 18:13:44 +0200

    evento en animacion caminarSinObjeto funciona

---
+++ Assets/[BaifosFarm]/Baifo/PasosBaifo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasosBaifo : MonoBehaviour
+{
+    Vector3 posicionPieDcho;
+    Vector3 posicionPieIzqdo;
+
+    public void DispararParticulasPieDcho()
+    {
+        Debug.Log("pieDcho");
+    }
+
+    public void DispararParticulasPieIzqdo()
+    {
+        Debug.Log("pieIzqdo");
+    }
+}
