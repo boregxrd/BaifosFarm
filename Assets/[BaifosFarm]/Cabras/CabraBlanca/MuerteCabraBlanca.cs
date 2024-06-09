ede4c53993f5748a17f2781ae72c29b1c05e98d8
mluimes <mario.luis.mesa2004@gmail.com>
2024-06-09 17:16:26 +0200

    Cabra grita, inicializa fx y se destruye

--- Assets/[BaifosFarm]/Cabras/CabraBlanca/MuerteCabraBlanca.cs
+++ Assets/[BaifosFarm]/Cabras/CabraBlanca/MuerteCabraBlanca.cs
@@ -1,18 +1,27 @@
-using System;
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public class MuerteCabraBlanca : MonoBehaviour
 {
-    [SerializeField] private BarraAlimento barraAlimento;
+    BarraAlimento barraAlimento;
     private Animator animator;
     [SerializeField] private GameObject muerteCabraPrefab; // Prefab que contiene la animaci�n de muerte
     private bool isDead = false;
+
+    AudioSource audioSource;
+    [SerializeField] AudioClip[] gritos;
+    [SerializeField] GameObject explosion;
+    MovimientoAleatorioCabras mov;
+
 
     private void Start()
     {
         barraAlimento = transform.GetComponentInChildren<BarraAlimento>();
         animator = GetComponentInChildren<Animator>();
+        audioSource = GetComponentInChildren<AudioSource>();
+        if(audioSource == null) Debug.LogError("audioSource null");
+        mov = GetComponent<MovimientoAleatorioCabras>();
     }
 
     private void Update()
@@ -28,36 +37,35 @@ public class MuerteCabraBlanca : MonoBehaviour
         isDead = true;
 
         animator.SetTrigger("Death");
-        
-        EsperarFinMuerte();
     }
 
-    private void InstanciarFX()
-    {
-        GameObject muerteCabra = Instantiate(muerteCabraPrefab, transform.position, Quaternion.identity);
-        
-        // ajustes de tamaño y posicion
-        muerteCabra.transform.localScale *= 0.5f;
-        muerteCabra.transform.position += Vector3.up * 2f;
-        
-        
-        // Rotar el objeto de muerte de la cabra para que mire hacia la c�mara
-        Vector3 dirToCamera = Camera.main.transform.position - muerteCabra.transform.position;
-        dirToCamera.y = 0f;
-        muerteCabra.transform.rotation = Quaternion.LookRotation(dirToCamera);
+    public void PlayGrito() {
+        mov.pararCabra(gameObject);
+        AudioClip sonidoRandom = gritos[Random.Range(0, gritos.Length)];
+        audioSource.PlayOneShot(sonidoRandom);
     }
 
-    private IEnumerator EsperarFinMuerte()
-    {
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-        float animationLength = stateInfo.length;
-        
-        yield return new WaitForSeconds(animationLength);
-        
-        InstanciarFX();
+    public void PlayExplosion() {
+        Instantiate(explosion, transform.position, transform.rotation);
 
         int blancasAntesDeMorir = PlayerPrefs.GetInt("cabrasBlancas", 0);
         PlayerPrefs.SetInt("cabrasBlancas", blancasAntesDeMorir - 1);
-        Destroy(gameObject); 
+        Destroy(gameObject);
     }
 }
+
+
+    // private void InstanciarFX()
+    // {
+    //     GameObject muerteCabra = Instantiate(muerteCabraPrefab, transform.position, Quaternion.identity);
+        
+    //     // ajustes de tamaño y posicion
+    //     muerteCabra.transform.localScale *= 0.5f;
+    //     muerteCabra.transform.position += Vector3.up * 2f;
+        
+        
+    //     // Rotar el objeto de muerte de la cabra para que mire hacia la c�mara
+    //     Vector3 dirToCamera = Camera.main.transform.position - muerteCabra.transform.position;
+    //     dirToCamera.y = 0f;
+    //     muerteCabra.transform.rotation = Quaternion.LookRotation(dirToCamera);
+    // }
