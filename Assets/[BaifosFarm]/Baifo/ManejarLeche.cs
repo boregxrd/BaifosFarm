f25a20a8a43d9d39016635b02584df1ee2e2240f
Alex <alexeltren@gmail.com>
2024-06-11 00:04:13 +0200

    ADR Baifo Dejando Leche

--- Assets/[BaifosFarm]/Baifo/ManejarLeche.cs
+++ Assets/[BaifosFarm]/Baifo/ManejarLeche.cs
@@ -8,18 +8,26 @@ public class ManejarLeche : MonoBehaviour
     private GameObject leche;
     Animator animator;
 
-    //para el tutorial
+    // para el tutorial
     public bool ordenyoRealizado = false;
+
+    // Referencias de los archivos de sonido
+    public AudioClip sonidoLeche1;
+    public AudioClip sonidoLeche2;
+    private AudioSource audioSource;
 
     private void Start()
     {
         jugador = GetComponent<Jugador>();
         animator = transform.GetChild(0).GetComponent<Animator>();
+
+        // Obtener el componente AudioSource
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void CogerLeche(GameObject prefabLeche)
     {
-        //para el tutorial
+        // para el tutorial
         ordenyoRealizado = true;
 
         jugador.LecheRecogida = true;
@@ -37,7 +45,17 @@ public class ManejarLeche : MonoBehaviour
         jugador.LecheRecogida = false;
         animator.SetTrigger("dejarObjeto");
 
-        //para el tutorial
+        // Reproducir un sonido aleatorio
+        ReproducirSonidoAleatorio();
+
+        // para el tutorial
         ordenyoRealizado = false;
+    }
+
+    private void ReproducirSonidoAleatorio()
+    {
+        int indice = Random.Range(0, 2); // Genera un número aleatorio 0 o 1
+        AudioClip clip = indice == 0 ? sonidoLeche1 : sonidoLeche2;
+        audioSource.PlayOneShot(clip);
     }
 }
