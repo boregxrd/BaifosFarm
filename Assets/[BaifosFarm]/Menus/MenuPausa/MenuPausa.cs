87ca932884cbf2bd082cc14a232c33f095770e1e
mluimes <mario.luis.mesa2004@gmail.com>
2024-06-10 12:31:49 +0200

    major fix canvas pausa y ajustes

--- Assets/[BaifosFarm]/Menus/MenuPausa/MenuPausa.cs
+++ Assets/[BaifosFarm]/Menus/MenuPausa/MenuPausa.cs
@@ -6,46 +6,20 @@ using UnityEngine.UI;
 public class MenuPausa : MonoBehaviour
 {
     [SerializeField] private GameObject menuAjustes; // Referencia al Canvas del menú de ajustes
-    public bool Pausa = false;
+    public bool pausa = false;
     public Texture2D cursorMano; // Textura del cursor de mano
     public Texture2D cursorNormal; // Textura del cursor normal
 
     void Start()
     {
-        gameObject.SetActive(false);
+        if(gameObject.activeSelf) gameObject.SetActive(false);
         Time.timeScale = 1; //el juego se reanuda
-    }
-
-    void Update()
-    {
-
-        // Verificar si el menú de ajustes está activo antes de procesar la entrada de la tecla ESC
-        if (menuAjustes.activeSelf)
-        {
-            if (Input.GetKeyDown(KeyCode.P))
-            {
-                CerrarMenuAjustes(); // Cerrar el menú de ajustes si se pulsa la tecla ESC
-            }
-            return; // Salir del método Update si el menú de ajustes está activo
-        }
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            if (Pausa == false)
-            {
-                Pausar();
-            }
-            else
-            {
-                Reanudar();
-            }
-        }
     }
 
     public void Pausar()
     {
         gameObject.SetActive(true);
-        Pausa = true;
+        pausa = true;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         Time.timeScale = 0; //el juego se pausa
@@ -54,13 +28,19 @@ public class MenuPausa : MonoBehaviour
     public void Reanudar()
     {
         gameObject.SetActive(false);
-        Pausa = false;
+        pausa = false;
         if(PlayerPrefs.GetInt("TutorialCompleto") == 1){
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
         Time.timeScale = 1; //el juego se reanuda
 
+    }
+
+    public bool ComprobarAjustes() {
+        if(menuAjustes != null) return false;
+        if (!menuAjustes.activeSelf) return false;
+        else return true;
     }
 
     public void AbrirMenuAjustes()
@@ -92,4 +72,6 @@ public class MenuPausa : MonoBehaviour
         // Cambiar el cursor a normal
         Cursor.SetCursor(cursorNormal, Vector2.zero, CursorMode.Auto);
     }
+
+    
 }
---
+++ Assets/[BaifosFarm]/Menus/MenuPausa/PausaController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausaController : MonoBehaviour
+{
+    [SerializeField] MenuPausa menuPausa;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.LogWarning("EY");
+            if (menuPausa.pausa == false)
+            {
+                menuPausa.Pausar();
+            }
+            else if (menuPausa.ComprobarAjustes())
+            {
+                menuPausa.CerrarMenuAjustes();
+            }
+            else
+            {
+                menuPausa.Reanudar();
+            }
+        }
+    }
+}
