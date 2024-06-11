de9ddcdac260b1e36b5cee6f156b2ff7415807ec
mluimes <mario.luis.mesa2004@gmail.com>
2024-06-11 13:47:41 +0200

    AudioListener apuntando a camara

---
+++ Assets/[BaifosFarm]/Baifo/ListenerFijarRotacion.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListenerFijarRotacion : MonoBehaviour
+{
+    [SerializeField] Transform baifoPos;
+    void Update()
+    {
+        Vector3 posicionCamara = Camera.main.transform.position;
+        Vector3 direccionCamara = posicionCamara - transform.position;
+        direccionCamara.y = 0;
+        transform.rotation = Quaternion.LookRotation(direccionCamara);
+        transform.position = baifoPos.position;
+    }
+}
