076fb0f83fee722bec5401ef197ef275d03644e0
mluimes <mario.luis.mesa2004@gmail.com>
2024-06-11 21:03:51 +0200

    ControladorDinero + cambios necesarios

---
+++ Assets/[BaifosFarm]/Dinero/ContadorDinero.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContadorDinero : MonoBehaviour
+{
+    // Singleton
+    private ContadorDinero instance;
+    public ContadorDinero Instance { get { return instance; } }
+
+    private int dinero;
+    public int Dinero { get => dinero; }
+
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    public void SumarDinero(int d) {
+        dinero += d;
+    }
+
+    public void RestarDinero(int d) {
+        dinero -= d;
+    }
+}
