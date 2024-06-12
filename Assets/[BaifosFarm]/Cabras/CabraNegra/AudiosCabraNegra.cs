9224b6574cd091de16cbba030a76a70d1158cb5e
mluimes <mario.luis.mesa2004@gmail.com>
2024-06-12 20:53:28 +0200

    Cabra Negra idle sonido

--- Assets/[BaifosFarm]/Cabras/CabraNegra/AudiosCabraNegra.cs
+++ Assets/[BaifosFarm]/Cabras/CabraNegra/AudiosCabraNegra.cs
@@ -7,9 +7,9 @@ public class AudiosCabraNegra : MonoBehaviour
     Animator animator;
     private AudioSource audioSource;
 
-    [SerializeField] AudioClip[] sonidoSaltitos; 
+    [SerializeField] AudioClip[] sonidoSaltitos;
     [SerializeField] AudioClip[] sonidosIdle;
-    [SerializeField] AudioClip[] sonidosMuerte; 
+    [SerializeField] AudioClip[] sonidosMuerte;
 
 
     void Start()
@@ -18,18 +18,24 @@ public class AudiosCabraNegra : MonoBehaviour
         if (audioSource == null) Debug.LogError("audioSource null");
     }
 
-    private void PlaySaltito() {
+    private void PlaySaltito()
+    {
         AudioClip sonidoRandom = sonidoSaltitos[Random.Range(0, sonidoSaltitos.Length)];
         audioSource.PlayOneShot(sonidoRandom);
     }
 
-    private void PlayIdle() {
-        AudioClip sonidoRandom = sonidosIdle[Random.Range(0, sonidosIdle.Length)];
-        audioSource.PlayOneShot(sonidoRandom);
+    private void PlayIdle()
+    {
+        if (Random.Range(0, 1) > .5f)
+        {
+            AudioClip sonidoRandom = sonidosIdle[Random.Range(0, sonidosIdle.Length)];
+            audioSource.PlayOneShot(sonidoRandom);
+        }
     }
 
-    private void PlayMuerte() {
+    private void PlayMuerte()
+    {
         AudioClip sonidoRandom = sonidosMuerte[Random.Range(0, sonidosMuerte.Length)];
-        audioSource.PlayOneShot(sonidoRandom);        
+        audioSource.PlayOneShot(sonidoRandom);
     }
 }
