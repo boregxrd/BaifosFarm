c334972bb31073474624dc51681acb27b633ee8f
Boregxrd <manuelborregales12@gmail.com>
2024-06-13 02:48:57 +0200

    no van los fokin botones

--- Assets/[BaifosFarm]/Finales/CanvasFinales.cs
+++ Assets/[BaifosFarm]/Finales/CanvasFinales.cs
@@ -3,12 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class CanvasFinales : MonoBehaviour
 {
+    [SerializeField] protected GameObject menuObject;
     protected string mainMenuSceneName = "Main";
     public Texture2D cursorMano; // Textura del cursor de mano
     public Texture2D cursorNormal; // Textura del cursor normal
 
+    private void Start() 
+    {
+        if (menuObject != null)
+        {
+            CanvasGroup canvasGroup = menuObject.GetComponent<CanvasGroup>();
+            if (canvasGroup != null)
+            {
+                canvasGroup.alpha = 1;  // Hacer visible
+                canvasGroup.interactable = true;  // Permitir interaccion
+                canvasGroup.blocksRaycasts = true;  // Permitir deteccion de rayos
+            }
+            menuObject.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Debug.Log("menuobject es null");
+        }
+    }
+
     public virtual void ReturnToMenu()
     {
+        Debug.Log("Attempting to load scene: " + mainMenuSceneName);
         SceneManager.LoadScene(mainMenuSceneName);
     }
 
--- Assets/[BaifosFarm]/[Scenes]/Factura/Factura.cs
+++ Assets/[BaifosFarm]/[Scenes]/Factura/Factura.cs
@@ -51,7 +51,7 @@ public class Factura : MonoBehaviour
         {
             OnGameOver?.Invoke();
         }
-        else if (contadorDinero.Dinero == 250)
+        else if (contadorDinero.Dinero == 90)
         {
             OnMoneyVictory?.Invoke();
         }
